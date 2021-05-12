1137889a3eb61547d3e2e3e2fb06c02cc2fd896c
Sam <samjoyce18@yahoo.com>
2021-05-12 14:29:55 +0100

    j2 added with positive and negative tests

--- challenges/src/com/nology/Main.java
+++ challenges/src/com/nology/Main.java
@@ -6,8 +6,5 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-//        System.out.println("running");
-        Challenge test = new Challenge();
-        System.out.println(test.calculateReturn(1000, 10, 0.1));
     }
 }
