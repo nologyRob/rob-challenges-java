cbc10488428bae92c30b9e31df1772407627e9a1
Sam <samjoyce18@yahoo.com>
2021-05-12 14:23:33 +0100

    j2 challenges added with positive and negative tests

--- challenges/src/com/nology/Main.java
+++ challenges/src/com/nology/Main.java
@@ -1,9 +1,13 @@
 package com.nology;
 
+import com.nology.j2.Challenge;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("running");
+//        System.out.println("running");
+        Challenge test = new Challenge();
+        System.out.println(test.calculateReturn(1000, 10, 0.1));
     }
 }
--- challenges/src/com/nology/j2/Challenge.java
+++ challenges/src/com/nology/j2/Challenge.java
@@ -17,6 +17,46 @@ public class Challenge {
     }
 
     /***
+     * write a method to display the middle character of a string.
+     * @param word the word that we want to check
+     * @return return the middle character... for example, passing in "Sam" would return "The middle character in the string: a"
+     */
+   public String findMiddleCharacter(String word) {
+       if (word.split(" ").length != 1) return "only one word pls.";
+
+       int position;
+       int length;
+
+       if (word.length() % 2 == 0) {
+           position = word.length() / 2 - 1;
+           length = 2;
+       } else {
+           position = word.length() / 2;
+           length = 1;
+       }
+       return word.substring(position, position + length);
+   }
+
+    /***
+     * We have a dog that likes to bark.  We need to wake up if the dog is barking at night!
+     * @param barking type boolean and be named barking it represents if our dog is currently barking.
+     * @param hourOfDay represents the hour of the day and is of type int with the name hourOfDay and has a valid range of 0-23.
+     * @return We have to wake up if the dog is barking before 8 or after 22 hours so in that case return true. In all other cases return false.
+     */
+    public boolean shouldWakeUp(boolean barking, int hourOfDay) {
+        if((hourOfDay < 0) || (hourOfDay > 23)) {
+            return false;
+        } else if(barking  && (hourOfDay < 8 || hourOfDay > 22)) {
+            return true;
+        }
+        return false;
+    }
+
+
+
+    // INTERMEDIATE
+
+    /***
      * Write a method called printMegaBytesAndKiloBytes that has 1 parameter of type int with the name kiloBytes.
      * Then it needs to print a message in the format "XX KB = YY MB and ZZ KB".
      * @param kiloBytes the number of kilobytes to be converted
@@ -32,18 +72,23 @@ public class Challenge {
         }
     }
 
+
+    // ADVANCED
+
     /***
-     * We have a dog that likes to bark.  We need to wake up if the dog is barking at night!
-     * @param barking type boolean and be named barking it represents if our dog is currently barking.
-     * @param hourOfDay represents the hour of the day and is of type int with the name hourOfDay and has a valid range of 0-23.
-     * @return We have to wake up if the dog is barking before 8 or after 22 hours so in that case return true. In all other cases return false.
+     * write a method to compute the future investment value at a given interest rate for a specified number of years.
+     * @param initialInvestment the starting value
+     * @param years the number of years that the investment will take
+     * @param rateOfInterest the rate at which, each year, the investment will increase by
+     * @return returns the total amount after x number of years (including the initial investment
      */
-    public boolean shouldWakeUp(boolean barking, int hourOfDay) {
-        if((hourOfDay < 0) || (hourOfDay > 23)) {
-            return false;
-        } else if(barking  && (hourOfDay < 8 || hourOfDay > 22)) {
-            return true;
+    public String calculateReturn(int initialInvestment, int years, double rateOfInterest) {
+        if (initialInvestment <= 0 || years <= 0) return "initialInvestment & years must be above 0.";
+        double total = initialInvestment;
+        for (int i = 0; i <= years; i++) {
+            total += (total * rateOfInterest);
+//            System.out.println(total);
         }
-        return false;
+        return "You now have " + Math.round(total);
     }
 }
