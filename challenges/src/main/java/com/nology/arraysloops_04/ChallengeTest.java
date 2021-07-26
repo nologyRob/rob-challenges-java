095a23674a2970ab324ea6c79f6eb8fa272f5ab5
Charlie Richardson <charlie.robin.richardson@gmail.com>
2021-07-26 14:12:25 +0100

    (fix): Changing Solution back to Challenge in test file

--- challenges/src/main/java/com/nology/arraysloops_04/ChallengeTest.java
+++ challenges/src/main/java/com/nology/arraysloops_04/ChallengeTest.java
@@ -7,11 +7,11 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ChallengeTest {
-    private Solution challenge;
+    private Challenge challenge;
 
     @BeforeEach
     void setUp() {
-        challenge = new Solution();
+        challenge = new Challenge();
     }
 
     @AfterEach
